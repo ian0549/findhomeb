241d6ce12667077d4cd04b95b7b545e1e6dc34bb
ian0549 <iannakoto@yahoo.com>
2018-08-04 09:47:33 -1200

    Add files via upload

---
+++ main/java/findhome/com/example/android/findhomeb/MainActivity.kt
@@ -0,0 +1,82 @@
+package findhome.com.example.android.findhomeb
+import android.content.res.Resources
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import findhome.com.example.android.findhomeb.R.id.nav_host_fragment
+import findhome.com.example.android.findhomeb.R.layout.activity_main
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+
+
+        bottom_navigation.labelVisibilityMode=1
+
+
+        val host: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+
+        val navController = host.navController
+
+
+
+        setupBottomNavMenu(navController)
+
+
+        navController.addOnNavigatedListener { _, destination ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+
+            Toast.makeText(this@MainActivity, "Navigated to $dest",
+                    Toast.LENGTH_SHORT).show()
+            Log.d("NavigationActivity", "Navigated to $dest")
+        }
+
+
+
+    }
+
+    override fun onSupportNavigateUp()
+            = findNavController(nav_host_fragment).navigateUp()
+
+
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)?.let { bottomNavView ->
+            NavigationUI.setupWithNavController(bottomNavView, navController)
+        }
+    }
+
+
+/*
+    @SuppressLint("PrivateResource")
+    private fun addFragment(fragment: Fragment) {
+
+        supportFragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+                .replace(R.id.content_framelayout, fragment, fragment.javaClass.simpleName)
+                .addToBackStack(null)
+                .commit()
+    }
+
+*/
+
+
+
+}
+
---
+++ main/java/findhome/com/example/android/findhomeb/PlaceAvailability.kt
@@ -0,0 +1,57 @@
+package findhome.com.example.android.findhomeb
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class PlaceAvailability : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_place_availability, container, false)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+                PlaceAvailability()
+    }
+}
